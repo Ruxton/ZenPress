6985bf4118d2317a85493e0fa46c903e4839e52d
Ruxton <ruxton@rhythmandpoetry.net>
2018-01-24 10:38:54 +0800

    Remove post-kind content generation

--- includes/indieweb-post-kinds.php
+++ includes/indieweb-post-kinds.php
@@ -19,6 +19,14 @@ function zenpress_post_kinds_init() {
 	if(method_exists('Kind_Taxonomy','get_icon')) {
 		add_filter('kind_icon_display','zenpress_kind_icon_display',10,2);
 	}
+
+	if(has_filter('the_content',array( 'Kind_View', 'content_response' )) {
+		remove_filter( 'the_content', array( 'Kind_View', 'content_response' ), 20 );
+	}
+
+	if(has_filter('the_content',array( 'Kind_View', 'excerpt_response' )) {
+		remove_filter( 'the_excerpt', array( 'Kind_View', 'excerpt_response' ), 20 );
+	}
 }
 
 if(!function_exists('zenpress_kind_icon_display')) {
